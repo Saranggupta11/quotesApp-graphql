4c5c24aa0f121949bb781aae9945d07178afda7a
saranggupta11 <sarang.gupta0811@gmail.com>
2022-06-06 16:51:19 +0530

    code refactoring

--- server.js
+++ server.js
@@ -1,37 +1,8 @@
-import {ApolloServer,gql} from "apollo-server"
+import {ApolloServer} from "apollo-server"
 import {ApolloServerPluginLandingPageGraphQLPlayground} from "apollo-server-core"
-import { quotes,users } from "./fakedb.js"
+import resolvers from "./resolvers.js"
+import typeDefs from "./schemaGql.js"
 
-const typeDefs=gql`
-    type Query {
-        users: [User]
-        user(id: ID!): User
-        quotes: [Quote]
-        iquote(by: ID!): [Quote]
-    }
-    type User {
-        id: ID!
-        firstName: String
-        lastName: String
-        email: String
-        quotes: [Quote]
-    }
-    type Quote {
-        by: ID
-        name: String
-    }
-`
-const resolvers={
-    Query:{
-        users:()=>users,
-        user:(_,{id})=>users.find(user=>user.id===id),
-        quotes:()=>quotes,
-        iquote:(_,{by})=>quotes.filter(quote=>quote.by===by)
-    },
-    User:{
-        quotes:(ur)=>quotes.filter(quote=>quote.by===ur.id)
-    }
-}
 const server=new ApolloServer({
     typeDefs,
     resolvers,
