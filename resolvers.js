0862367eccdb30d809bbea1dace622d74785702a
saranggupta11 <sarang.gupta0811@gmail.com>
2022-06-06 17:15:37 +0530

    mutation in graphql

--- resolvers.js
+++ resolvers.js
@@ -1,15 +1,25 @@
-import { quotes,users } from "./fakedb.js"
-
-const resolvers={
-    Query:{
-        users:()=>users,
-        user:(_,{id})=>users.find(user=>user.id===id),
-        quotes:()=>quotes,
-        iquote:(_,{by})=>quotes.filter(quote=>quote.by===by)
+import { quotes, users } from "./fakedb.js";
+import randomstring from "randomstring";
+const resolvers = {
+  Query: {
+    users: () => users,
+    user: (_, { id }) => users.find((user) => user.id === id),
+    quotes: () => quotes,
+    iquote: (_, { by }) => quotes.filter((quote) => quote.by === by),
+  },
+  User: {
+    quotes: (ur) => quotes.filter((quote) => quote.by === ur.id),
+  },
+  Mutation: {
+    signupUser: (_, {userNew}) => {
+      const id = randomstring.generate(7);
+      users.push({
+          id,
+          ...userNew
+      })
+      return users.find((user) => user.id === id)
     },
-    User:{
-        quotes:(ur)=>quotes.filter(quote=>quote.by===ur.id)
-    }
-}
+  },
+};
 
-export default resolvers
+export default resolvers;
--- schemaGql.js
+++ schemaGql.js
@@ -11,11 +11,21 @@ type User {
     firstName: String
     lastName: String
     email: String
+    password:String
     quotes: [Quote]
 }
 type Quote {
     by: ID
     name: String
+}
+type Mutation {
+    signupUser(userNew:UserInput!): User
+}
+input UserInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    password:String!
 }
 `
 export default typeDefs
